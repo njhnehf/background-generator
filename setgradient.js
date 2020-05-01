dc9473a3db2de8c35cd0956e35d1195a0c81a945
njhnehf <63651656+njhnehf@users.noreply.github.com>
2020-05-01 11:06:31 +0300

    browserify testing

--- setgradient.js
+++ setgradient.js
@@ -1,3 +1,8 @@
+var _ = require('lodash');
+console.log(_);
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3));
 // JavaScript Document
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
